7a92a7cc0da16f94abfecfb18f3b68376165d423
sakilanjum99 <sakilanjum99@gmail.com>
2022-04-10 19:09:18 +0600

    login added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'
+const Login = () => {
+    return (
+        <div className='form-container'>
+          <div>
+          <h2 className='form-title'>Login</h2>
+          </div>
+        </div>
+    );
+};
+
+export default Login;
